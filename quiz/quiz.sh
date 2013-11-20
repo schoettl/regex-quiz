b086dcec64b5f65c30ebbcd88a2a2af1da900cfb
Jakob Schöttl <jschoett@gmail.com>
2013-11-20 10:23:53 +0100

    Paar Zeile vom Skript

--- quiz/quiz.sh
+++ quiz/quiz.sh
@@ -4,4 +4,19 @@
 # 
 # 
 
+if [ $# -gt 1 ]; then
+	I="$1"
+	FILE="$2"
+else
+	I=1
+	FILE="$1"
+fi
 
+echo "Quiz: $FILE"
+echo "ab Frage Nummer $I"
+
+echo Alle Fragen:
+grep ^# "$FILE"
+
+echo Fragen:
+grep ^# "$FILE" | sed "$1,"
