25c8a09b5017979f342d1287d63f7bf83440dac3
Jakob Schöttl <jschoett@gmail.com>
2013-11-21 20:20:10 +0100

    Aenderungen zum Weiterarbeiten

--- quiz/quiz.sh
+++ quiz/quiz.sh
@@ -18,5 +18,12 @@ echo "ab Frage Nummer $I"
 echo Alle Fragen:
 grep ^# "$FILE"
 
-echo Fragen:
-grep ^# "$FILE" | sed "$1,"
+while true; do
+	ID=$(grep ^# "$FILE" | awk "NR==$I")
+	echo ID: $ID
+	awk "/^$ID/ , /^#/" "$FILE"
+	#break
+	# Increment ID
+	(( I++ ))
+done
+#cat quizfile.txt | awk 'BEGIN {f=0}; {if(f)print}; /^#/ {if(/bin/bash~/^#RE-13$/) f=1; else f=0}' | sed '$ d'
