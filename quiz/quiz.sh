efdffb65ce59b156bc16a5159cc548c4459aceec
Jakob Schöttl <jschoett@gmail.com>
2013-11-23 22:42:43 +0100

    Functions ausprobiert

--- quiz/quiz.sh
+++ quiz/quiz.sh
@@ -4,6 +4,22 @@
 # 
 # 
 
+# param1: quizfile
+# param2: question id
+function outputFullQuestion() {
+	cat "$1" | awk '/^#/{f=0};/^'"$2"'[[:space:]]*$/||f{f=1;print}'
+	# [:space:] ist ein Workaround, wahrscheinlich nötig wegen CRLF
+}
+
+# param1: quizfile
+# param2: question id
+function outputQuestionWithoutAnswer() {
+	outputFullQuestion "$1" "$2" | \
+	sed 's/^\[x]/[_]/' | sed 's/^(x)/(_)/' | \
+	sed 's/^\(____*\).*/\1/' | sed 's/0\/1.*/0\/1/' | \
+	awk '/^(\[_]|\(_\)|___+|0\/1)/{f=1};f&&/^[[:space:]]*$/{g=1};!g{print}'
+}
+
 if [ $# -gt 1 ]; then
 	I="$1"
 	FILE="$2"
@@ -15,15 +31,14 @@ fi
 echo "Quiz: $FILE"
 echo "ab Frage Nummer $I"
 
-echo Alle Fragen:
-grep ^# "$FILE"
-
 while true; do
 	ID=$(grep ^# "$FILE" | awk "NR==$I")
 	echo ID: $ID
-	awk "/^$ID/ , /^#/" "$FILE"
-	#break
-	# Increment ID
+	outputQuestionWithoutAnswer "$FILE" "$ID"
+	break
+	if [ $? -ne 0 ]; then
+		break
+	fi
+	clear
 	(( I++ ))
 done
-#cat quizfile.txt | awk 'BEGIN {f=0}; {if(f)print}; /^#/ {if(/bin/bash~/^#RE-13$/) f=1; else f=0}' | sed '$ d'
