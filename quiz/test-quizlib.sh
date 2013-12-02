4269929a7c98ff591633907c7fd0dfedcbd8ab23
Jakob Schöttl <jschoett@gmail.com>
2013-12-02 14:47:13 +0100

    TODO and test script added

---
+++ quiz/test-quizlib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# test-quizlib.sh
+# 
+# Jakob Schöttl
+# 
+
+source quizlib.sh
+
+echo ---------------
+#outputOptionsAnswered quizfile.txt "#RE-1" "[x]" "3 1 4"
+#outputAnswer quizfile.txt "#RE-8"
+outputExplanation quizfile.txt "#RE-13"
+echo ---------------
