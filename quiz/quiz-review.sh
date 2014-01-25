41c499f4c599823425161babe6556cec64b29dac
Jakob Schöttl <jschoett@gmail.com>
2014-01-25 17:57:39 +0100

    Started support for options -i, -v, -s

--- quiz/quiz-review.sh
+++ quiz/quiz-review.sh
@@ -9,6 +9,10 @@ source quizlib.sh
 FILE="$1"
 ANSFILE="answers.txt"
 
+INTERACTIVE=true
+VERBOSE=false
+SUMMARY=false
+
 CORRECT="Richtig"
 INCORRECT="Falsch"
 
@@ -16,36 +20,38 @@ echo "Quiz: $FILE"
 echo "Your answers: $ANSFILE"
 echo -e "----------------------\n"
 
-cat "$ANSFILE" | sort | uniq | \
-while read -r ID ANS; do
-	TYPE=$(outputQuestionType "$FILE" "$ID")
-	echo "ID: $ID"
-	echo "TYPE: $TYPE"
-	echo
+if $INTERACTIVE; then
+	cat "$ANSFILE" | sort | uniq | \
+	while read -r ID ANS; do
+		TYPE=$(outputQuestionType "$FILE" "$ID")
+		echo "ID: $ID"
+		echo "TYPE: $TYPE"
+		echo
 
-	case $TYPE in
-		"[x]"|"(x)") echo "usr sol  (usr: Ihre Antwort, sol: Korrekte Antwort)"
-				outputOptionsAnswered "$FILE" "$ID" "$TYPE" "$ANS" ;;
-		"0/1"|"___") echo "Ihre Antwort: $ANS" ;;
-	esac
+		case $TYPE in
+			"[x]"|"(x)") echo "usr sol  (usr: Ihre Antwort, sol: Korrekte Antwort)"
+					outputOptionsAnswered "$FILE" "$ID" "$TYPE" "$ANS" ;;
+			"0/1"|"___") echo "Ihre Antwort: $ANS" ;;
+		esac
 
-	echo
-	if checkAnswer "$FILE" "$ID" "$TYPE" "$ANS"; then
-		echo $CORRECT
-	else
-		echo $INCORRECT
-	fi
+		echo
+		if checkAnswer "$FILE" "$ID" "$TYPE" "$ANS"; then
+			echo $CORRECT
+		else
+			echo $INCORRECT
+		fi
 
-	echo
-	outputExplanation "$FILE" "$ID"
+		echo
+		outputExplanation "$FILE" "$ID"
 
-	echo "(Vermeintliche) Fehler :P gerne formlos melden an"
-	echo -e "Jakob Schöttl <jschoett@gmail.com>\n"
+		echo "(Vermeintliche) Fehler :P gerne formlos melden an"
+		echo -e "Jakob Schöttl <jschoett@gmail.com>\n"
 
-	read -rp "Weiter (Enter) oder Beenden (q)? " CMD < /dev/tty
-	case "$CMD" in
-		q|Q) exit
-		;;
-	esac
-	clear
-done
+		read -rp "Weiter (Enter) oder Beenden (q)? " CMD < /dev/tty
+		case "$CMD" in
+			q|Q) exit
+			;;
+		esac
+		clear
+	done
+fi
